8118198deda00cded3a3fa8e27b4ac2f60280667
hillol-hi <hillol7h7@gmail.com>
2025-12-18 21:14:13 +0600

    Add files via upload

---
+++ 1_unknown_number_sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int sum = 0, value = 0;
+    //cout<<"Enter numbers: "<<endl;
+
+    while(std::cin>>value)
+        sum+=value;
+    std::cout<<"Sum is: "<<sum<<std::endl;
+
+    return 0;
+}
